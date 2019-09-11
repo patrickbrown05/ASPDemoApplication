d073714bb5ccba3846684e8edf263de76c09d0a3
Patrick Brown <phvbwork@gmail.com>
2019-09-11 11:07:34 -0400

    Add project files.

---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,41 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            /*personModel p = new personModel();
+            int i = 5 / p.age;*/
+
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<personModel> people = new List<personModel>();
+            people.Add(new personModel { FirstName = "Patrick", LastName = "Brown", age = 22 });
+            people.Add(new personModel { FirstName = "Bruce", LastName = "Wayne", age = 35 });
+            people.Add(new personModel { FirstName = "Sarah", LastName = "Sanders", age = 53 });
+
+            return View(people);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/Models/personModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class personModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; } = 0;
+        public bool isAlive { get; set; } = true;
+    }
+}
